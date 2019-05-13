86327010b768d3791aebeddc17dce8d51e9807fa
gasd238 <gasd238@naver.com>
2019-05-13 13:10:49 +0900

    심심이 기능 아마 완성해가는중

--- bot_main.py
+++ bot_main.py
@@ -4,9 +4,11 @@ import re
 from module.help import *
 from module.check import *
 from module.save import *
+from module.simsim import *
 
 client = discord.Client()
 
+stat_chk = [0]
 
 @client.event
 async def on_ready():
@@ -32,27 +34,36 @@ async def on_message(message):
         elif save_stat == 1:
             embed = discord.Embed(title='추가하기', description='저장할 id, 비밀번호와 그것의 사이트를 입력해 주세요')
             embed.add_field(name = '예시', value = '옥션 abcd1234 1234567')
-            await client.send_message(message.channel, embed = embed)
+            mudel = await client.send_message(message.channel, embed = embed)
             response = await client.wait_for_message(timeout=float(15), author=message.author, channel=message.channel)
 
             if response == None:
                 await client.send_message(message.channel, '제대로 입력하지 않았습니다. 다시 시도해 주세요')
                 return
-            
-            content = response.content.split(' ')
-            hangul = re.compile('[ㄱ-ㅣ가-힣]+')
-            if len(content) != 3 or hangul.findall(content[1]) != [] or hangul.findall(content[2]) != []:
-                await client.send_message(response.channel, '제대로 입력하지 않았습니다. 다시 시도해 주세요')
-
             else:
-                info_add(content, response.author.id)
+                await client.delete_message(response)
+                await client.delete_message(mudel)
+                content = response.content.split(' ')
+                hangul = re.compile('[ㄱ-ㅣ가-힣]+')
+                if len(content) != 3 or hangul.findall(content[1]) != [] or hangul.findall(content[2]) != []:
+                    await client.send_message(response.channel, '제대로 입력하지 않았습니다. 다시 시도해 주세요')
+
+                else:
+                    info_add(content, response.author.id)
         
         elif save_stat == 2:
             embed = discord.Embed(title='삭제하기', description='삭제할 정보의 사이트를 입력해 주세요')
-            
+            mudel = await client.send_message(message.channel, embed = embed)
+    if message.content == 'sj 심심이' and message.channel.id == '577332815415476244':
+        await client.send_message(message.channel, '심심이 작동 시작!')
+        stat_chk[0] = 1
+    
+    if stat_chk[0] == 1 and message.channel.id == '577332815415476244' and message.content != '' and message.content != 'sj 심심이':
+        await client.send_message(message.channel, embed = find_res(message.content))
+
 
                 
 
         
 
-client.run('token')
+client.run('NTY5NzY4NjgzMjMzMzQ1NTM3.XNjZJg.e8xjRhj1CmewVXTYCk9tPMGuW9U')
---
+++ module/simsim.py
@@ -0,0 +1,22 @@
+import json
+from random import randint
+import discord
+
+def open_json():
+    with open('Data/saved.json', 'r', encoding='utf-8') as userdata:
+        data = userdata.read()
+    data = json.loads(data)
+    return data
+
+def save_res():
+    print(1)
+
+def find_res(req):
+    data = open_json()
+    # try:
+    num = randint(0, len(data['response'][str(req)])-1)
+    embed = discord.Embed(title="답변", description=data['response'][str(req)][num])
+    return embed
+    # except:
+    #     embed = discord.Embed(title="답변 없음", description="답변이 없습니다 추가하시겠습니까?")
+    #     return embed
