06c6251e2664677bb6296c3815cdd14a3900d9b2
Bluerain <41102800+gasd238@users.noreply.github.com>
2019-06-03 15:25:29 +0900

    Update save.py

--- module/save.py
+++ module/save.py
@@ -1,9 +1,13 @@
 #-------------------------------------------------------------------------------------------
-
+import getpass
+import json
+import os
+from collections import OrderedDict
+user = getpass.getuser()
+fileDir = "C:/Users/" + user + "/AppData/Local/DiscordChatbot/UserPrivacy.json"
 #폴더 만들기
 def CreateDiscordChatbotFolder():
-    import os
-    folderDir = "C:/Users/user/AppData/Local/DiscordChatbot"
+    folderDir = "C:/Users/" + user + "/AppData/Local/DiscordChatbot"
     if not os.path.isdir(folderDir):
         os.mkdir(folderDir)
     else:
@@ -11,14 +15,11 @@ def CreateDiscordChatbotFolder():
 
 #파일 만들기
 def CreateUserPrivacyfile():
-    import os
-    import json
     from collections import OrderedDict
     file_data = OrderedDict()
 
     #file_data["GMARKET"]={"ID": "yoyowang0614", "PW":"gksruf0979", "data":"gksruf0979"}
-
-    fileDir = "C:/Users/user/AppData/Local/DiscordChatbot/UserPrivacy.json"
+    
     if not os.path.isfile(fileDir):
         with open(fileDir, 'w', encoding='utf-8') as make_file:
             json.dump(file_data, make_file, ensure_ascii=False, indent='\t')
@@ -36,32 +37,25 @@ def CreateFileToBeSavedPrivacy():
 
 #원하는 사이트의 아이디 비밀번호를 파일에서 불러오고 객체 리턴 
 def ReadUserPrivacySelect(siteName):
-        import json
-        import os
-        fileDir = "C:/Users/user/AppData/Local/DiscordChatbot/UserPrivacy.json"
-        siteName = str(siteName)
-        if os.path.isfile(fileDir):
-                with open(fileDir) as jsonFile:
-                        jsonData = json.load(jsonFile)
-                        if siteName in jsonData:
-                                jsonString = jsonData[siteName]
-                                return jsonString
-                        else:
-                                return False
-        else:
+    siteName = str(siteName)
+    if os.path.isfile(fileDir):
+        with open(fileDir, 'r', encoding='utf-8') as jsonFile:
+            jsonData = json.load(jsonFile)
+            if siteName in jsonData:
+                return jsonData[siteName]
+            else:
                 return False
+    else:
+        return False
 
 #모든 사이트의 아이디 비밀번호를 파일에서 불러오고 객체 리턴
 def ReadUserPrivacySelectAll():
-        import json
-        import os
-        fileDir = "C:/Users/user/AppData/Local/DiscordChatbot/UserPrivacy.json"
-        if os.path.isfile(fileDir):
-                with open(fileDir) as jsonFile:
-                        jsonData = json.load(jsonFile)
-                        return jsonData
-        else:
-                return False
+    if os.path.isfile(fileDir):
+        with open(fileDir, 'r', encoding='utf-8') as jsonFile:
+            jsonData = json.load(jsonFile)
+        return jsonData
+    else:
+        return False
 
 # userPrivacy=ReadUserPrivacySelect('GMARKET')
 # print(userPrivacy)
@@ -70,23 +64,23 @@ def ReadUserPrivacySelectAll():
 
 #아이디 비밀번호를 파일에 추가, 수정
 def AddUserPrivacy(siteName, ID, PW, etc):
-        import os
-        import json
-        from collections import OrderedDict
-        siteName = str(siteName)
-        ID = str(ID)
-        PW = str(PW)
-        etc = str(etc)
-        fileDir = "C:/Users/user/AppData/Local/DiscordChatbot/UserPrivacy.json"
-        if os.path.isfile(fileDir):
-                userPrivacy = ReadUserPrivacySelectAll()
-                userPrivacy[siteName] = {"id":ID, "PW":PW, "etc":etc}
-                jstring = json.dumps(userPrivacy, indent='\t')
-                f = open(fileDir, "w", encoding='utf-8')
-                f.write(jstring)
-                f.close()
-        else:
-                return False
+    if etc == None:
+        etc = "없음"
+    siteName = str(siteName)
+    ID = str(ID)
+    PW = str(PW)
+    etc = str(etc)
+    if os.path.isfile(fileDir):
+        userPrivacy = ReadUserPrivacySelectAll()
+        userPrivacy[siteName] = {"id":ID, "PW":PW, "etc":etc}
+        try:
+            with open(fileDir, 'w', encoding='utf-8') as userinfo:
+                json.dump(userPrivacy, userinfo, ensure_ascii=False, indent="\t")
+        except:
+            return "저장 실패"
+        return "저장 성공"
+    else:
+        return "저장 실패"
 
 # AddUserPrivacy("GG", "D", "B", "3")
 # userPrivacy=ReadUserPrivacySelectAll()
@@ -96,23 +90,21 @@ def AddUserPrivacy(siteName, ID, PW, etc):
 
 #아이디 비밀번호를 삭제
 def DeleteUserPrivacy(siteName):
-        import os
-        import json
-        from collections import OrderedDict
-        siteName = str(siteName)
-        fileDir = "C:/Users/user/AppData/Local/DiscordChatbot/UserPrivacy.json"
-        if os.path.isfile(fileDir):
-                userPrivacy = ReadUserPrivacySelectAll()
-                if siteName in userPrivacy:
-                        del userPrivacy[siteName]
-                        jstring = json.dumps(userPrivacy, indent='\t')
-                        f = open(fileDir, "w", encoding='utf-8')
-                        f.write(jstring)
-                        f.close()
-                else:
-                        return False
+    siteName = str(siteName)
+    if os.path.isfile(fileDir):
+        userPrivacy = ReadUserPrivacySelectAll()
+        if siteName in userPrivacy:
+            del userPrivacy[siteName]
+            try:
+                with open(fileDir, 'w', encoding='utf-8') as userinfo:
+                    json.dump(userPrivacy, userinfo, ensure_ascii=False, indent="\t")
+            except:
+                return "삭제 실패"
+            return "삭제 성공"
         else:
-                return False
+            return "삭제할 사이트가 발견되지 않음"
+    else:
+        return "삭제 실패"
 
 #-------------------------------------------------------------------------------------------
 
