b09dced6f158abf8287f58206c9198b151150bfb
gasd238 <gasd238@naver.com>
2019-06-14 10:48:50 +0900

    발표 따로 뺌

--- module/simsim.py
+++ module/simsim.py
@@ -2,18 +2,29 @@ import json
 from random import randint
 import discord
 
+#json 파일 열기
 def open_json():
     with open('Data/saved.json', 'r', encoding='utf-8') as resList:
         data = resList.read()
     data = json.loads(data)
     return data
 
-def add_res_new(req, res):
-    data = open_json()
-    data['response'][req] = [str(res)]
+#json 파일 저장
+def save_json(data):
     with open('Data/saved.json', 'w', encoding='utf-8') as resList:
         json.dump(data, resList, ensure_ascii=False, indent="\t")
 
+#json 새로운거 추가
+def add_res_new(req, res):
+    data = open_json()
+    try:
+        data['response'][req] = [str(res)]
+    except:
+        data["response"] = {}
+        data['response'][req] = [str(res)]
+    save_json(data)
+    
+#json 이미 있는 답변에 추가
 def add_res(res):
     response = ''
     data = open_json()
@@ -23,12 +34,20 @@ def add_res(res):
             break
         response +=res[i] +' '
     try:
-        data['response'][res[0]].append(str(response))
+        try:
+            data['response'][res[0]].append(str(response))
+        except:
+            data['response'][res[0]] = [str(response)]
     except:
-        data['response'][res[0]] = [str(response)]
-    with open('Data/saved.json', 'w', encoding='utf-8') as resList:
-        json.dump(data, resList, ensure_ascii=False, indent="\t")
+        data["response"] = {}
+        try:
+            data['response'][res[0]].append(str(response))
+        except:
+            data['response'][res[0]] = [str(response)]
+    
+    save_json(data)
 
+#들은말에 답변 찾아 보내기
 def find_res(req):
     data = open_json()
     try:
@@ -39,6 +58,7 @@ def find_res(req):
         embed = discord.Embed(title="답변 없음", description="답변이 없습니다 추가하시겠습니까? y/n")
         return embed, -1
 
+#들은말에 답변 찾아 삭제
 def del_res(re):
     response = ''
     for i in range(1, len(re)):
@@ -49,8 +69,7 @@ def del_res(re):
     data = open_json()
     try:
         data['response'][re[0]].remove(response)
-        with open('Data/saved.json', 'w', encoding='utf-8') as resList:
-            json.dump(data, resList, ensure_ascii=False, indent="\t")
+        save_json(data)
         return 1
     except:
         return -1
